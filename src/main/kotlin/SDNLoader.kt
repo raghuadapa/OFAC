c7c22a01589c314fcc49470d3a469e7e4c380056
Eugene Beliaikine <EBeliaikine@routeone.com>
2017-07-28 14:54:21 -0400

    Refactor: unnecessary code

--- src/main/kotlin/SDNLoader.kt
+++ src/main/kotlin/SDNLoader.kt
@@ -6,7 +6,7 @@ import javax.xml.bind.JAXBElement
 class SDNLoader {
 
     fun load(filePath: String): SdnListType {
-        val jc = JAXBContext.newInstance(SdnListType::class.java!!)
+        val jc = JAXBContext.newInstance(SdnListType::class.java)
         val unmarshaller = jc.createUnmarshaller()
         val sdnList = unmarshaller.unmarshal(File(filePath)) as JAXBElement<*>
 
--- src/test/kotlin/SDNLoaderTest.kt
+++ src/test/kotlin/SDNLoaderTest.kt
@@ -7,9 +7,9 @@ class SDNLoaderTest {
 
     @Test
     fun load_whenValidPath_returnsSDNList() {
-        var subject: SDNLoader = SDNLoader()
+        val subject = SDNLoader()
 
-        val sdnList = subject.load(this.javaClass.getResource("/sdn.xml").path)
+        val sdnList = subject.load(javaClass.getResource("/sdn.xml").path)
 
         assertEquals(sdnList.sdnEntry.size, 5906)
     }
