148a67b2bcf9c75c5a60d1e9ccbd1227a5b45643
Madhavi Adluru <adlurumadhavi@gmail.com>
2017-08-11 14:53:18 -0400

    added ScheduledTasks to display time every 2 min

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,8 +2,10 @@ package com.routeone.ofac
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class Application
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/ScheduledTasks.kt
@@ -0,0 +1,22 @@
+package com.routeone.ofac
+
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Component
+class ScheduledTasks {
+
+    private val log = LoggerFactory.getLogger(ScheduledTasks::class.java)
+
+    private val dateFormat = SimpleDateFormat("HH:mm:ss")
+
+    @Scheduled(fixedRate = 120000)
+    fun reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(Date()))
+    }
+
+}
+
