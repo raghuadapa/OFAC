a8b5f656d02dc5932c77e3f231919f95b2fb2f18
MeIr <info@meirz.net>
2017-08-12 20:38:19 -0400

    Add: Integration Test for health check

---
+++ src/test/kotlin/app/controller/AppControllerTest.kt
@@ -0,0 +1,28 @@
+package app.controller
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus.OK
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+class AppControllerTest {
+
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+    @Test
+    fun healthCheck_whenCalled_returnsOk() {
+        val actual = testRestTemplate.getForEntity("/health", String::class.java)
+
+        assertEquals(OK, actual.statusCode)
+        assertEquals("ok", actual.body)
+    }
+
+}
